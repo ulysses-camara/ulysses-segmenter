a9871572a25768c009d0f25a7955768ab518245b
felsiq <felipe.siqueira@usp.br>
2022-02-21 11:21:46 -0300

    style: apply black in test code

--- notebooks/tests/conftest.py
+++ notebooks/tests/conftest.py
@@ -10,7 +10,11 @@ from config import *
 
 
 REGISTERED_TEST_CASES_URI = "./registered_test_cases.csv"
-REGISTERED_TEST_CASES_COLUMNS = ("document_index", "expected_segment_count", "expected_noise_count")
+REGISTERED_TEST_CASES_COLUMNS = (
+    "document_index",
+    "expected_segment_count",
+    "expected_noise_count",
+)
 TEST_CASES: dict[int, tuple[int, int]] = dict()
 
 
@@ -19,7 +23,9 @@ def load_registered_cases(test_cases_uri: str = REGISTERED_TEST_CASES_URI) -> No
         print(f"No test cases found at '{test_cases_uri}'.")
         return
 
-    df = pd.read_csv(test_cases_uri, usecols=REGISTERED_TEST_CASES_COLUMNS, index_col=None)
+    df = pd.read_csv(
+        test_cases_uri, usecols=REGISTERED_TEST_CASES_COLUMNS, index_col=None
+    )
 
     for i, (idx, *expected_values) in df.iterrows():
         TEST_CASES[idx] = expected_values
@@ -42,11 +48,15 @@ def clear_registered_cases() -> None:
     TEST_CASES.clear()
 
 
-def update_test_case(document_idx: int, expected_segment_count: t.Union[int, tuple[int, ...]]) -> None:
+def update_test_case(
+    document_idx: int, expected_segment_count: t.Union[int, tuple[int, ...]]
+) -> None:
     if not hasattr(expected_segment_count, "__len__"):
         expected_segment_count = (expected_segment_count,)
 
-    assert len(expected_segment_count) == len(REGISTERED_TEST_CASES_COLUMNS) - 1, f"Got length: {len(expected_segment_count)}"
+    assert (
+        len(expected_segment_count) == len(REGISTERED_TEST_CASES_COLUMNS) - 1
+    ), f"Got length: {len(expected_segment_count)}"
     TEST_CASES[document_idx] = tuple(expected_segment_count)
 
 
@@ -54,48 +64,57 @@ def test_case_exists(document_idx: int) -> bool:
     return document_idx in TEST_CASES
 
 
-def print_results(df, id_: int, keyword_filters: t.Union[set[str], str, None] = None, print_full_text: bool = True) -> tuple[int, int]:
+def print_results(
+    df,
+    id_: int,
+    keyword_filters: t.Union[set[str], str, None] = None,
+    print_full_text: bool = True,
+) -> tuple[int, int]:
     tokens = df["train"][id_]["tokens"]
     labels = df["train"][id_]["labels"]
-    
+
     if print_full_text:
         print(" ".join(df["train"][id_]["tokens"]))
         print(64 * "_", end="\n\n")
-    
+
     sentence: list[str] = []
     segment_count = int(len(tokens) > 0)
     noise_count = 0
-    
+
     c_color = colorama.Fore.LIGHTWHITE_EX
     c_end = colorama.Fore.RESET
-    
+
     if isinstance(keyword_filters, str):
         keyword_filters = {keyword_filters}
-    
+
     if keyword_filters is not None and not isinstance(keyword_filters, set):
         keyword_filters = set(keyword_filters)
-    
+
     for tok, lab in zip(tokens, labels):
         if lab == SPECIAL_SYMBOLS[MARKER_VALID]:
-            if (not keyword_filters or not keyword_filters.isdisjoint(map(str.lower, sentence))):
+            if not keyword_filters or not keyword_filters.isdisjoint(
+                map(str.lower, sentence)
+            ):
                 print(c_color, segment_count, c_end, " ".join(sentence), end="\n\n")
-                
+
             sentence = []
             segment_count += 1
-        
+
         if lab == SPECIAL_SYMBOLS[MARKER_NOISE_START]:
             tok = colorama.Fore.RED + tok
             noise_count += 1
-            
+
         if lab == SPECIAL_SYMBOLS[MARKER_NOISE_END]:
             tok = colorama.Fore.RESET + tok
-        
+
         sentence.append(tok)
-       
-    if sentence and (not keyword_filters or not keyword_filters.isdisjoint(map(str.lower, sentence))):
+
+    if sentence and (
+        not keyword_filters or not keyword_filters.isdisjoint(map(str.lower, sentence))
+    ):
         print(c_color, segment_count, c_end, " ".join(sentence), end="\n\n")
-    
+
     print(colorama.Fore.RESET)
     print(f"Idx/Segment count, noise count:   {id_}: {segment_count}, {noise_count}")
-    
+
     return segment_count, noise_count
--- notebooks/tests/test_data_labeling_regex.py
+++ notebooks/tests/test_data_labeling_regex.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("..")
 
 from . import conftest
@@ -10,20 +11,30 @@ def run_tests(labels: list[list[int]]) -> None:
     skipped = 0
 
     incorrect_cases_inds: list[str] = []
-    
+
     if not conftest.TEST_CASES:
         return
-    
-    for idx, (expected_segment_count, expected_noise_count) in conftest.TEST_CASES.items():
+
+    for idx, (
+        expected_segment_count,
+        expected_noise_count,
+    ) in conftest.TEST_CASES.items():
         if idx >= len(labels):
             skipped += 1
             continue
-            
-        total_segments = int(len(labels[idx]) > 0)
-        total_segments += sum(lab == SPECIAL_SYMBOLS[MARKER_VALID] for lab in labels[idx])
-        total_noise = sum(lab == SPECIAL_SYMBOLS[MARKER_NOISE_START] for lab in labels[idx])
 
-        if total_segments == expected_segment_count and total_noise == expected_noise_count:
+        total_segments = int(len(labels[idx]) > 0)
+        total_segments += sum(
+            lab == SPECIAL_SYMBOLS[MARKER_VALID] for lab in labels[idx]
+        )
+        total_noise = sum(
+            lab == SPECIAL_SYMBOLS[MARKER_NOISE_START] for lab in labels[idx]
+        )
+
+        if (
+            total_segments == expected_segment_count
+            and total_noise == expected_noise_count
+        ):
             correct += 1
             continue
 
@@ -31,10 +42,10 @@ def run_tests(labels: list[list[int]]) -> None:
 
     n_tests = len(conftest.TEST_CASES)
     correct_prop = correct / n_tests
-    
+
     print(
-        f"Correct proportion: {100. * correct_prop:.2f}% ({correct} of {n_tests})" +
-        (f", {skipped} tests skipped." if skipped > 0 else "")
+        f"Correct proportion: {100. * correct_prop:.2f}% ({correct} of {n_tests})"
+        + (f", {skipped} tests skipped." if skipped > 0 else "")
     )
 
     assert correct == n_tests - skipped, f"Incorrect: {', '.join(incorrect_cases_inds)}"
