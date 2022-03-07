2deed87aa32436a3731ab35593e8f36a2dcbace2
felsiq <felipe.siqueira@usp.br>
2022-03-07 17:09:39 -0300

    update: add code for baseline model training

--- src/segmentador.py
+++ src/segmentador.py
@@ -3,6 +3,8 @@ import typing as t
 import re
 
 import transformers
+import torch
+import torch.nn
 
 
 class Segmenter:
@@ -16,11 +18,21 @@ class Segmenter:
         uri_tokenizer: t.Optional[str] = None,
         num_labels: int = 4,
         local_files_only: bool = True,
+        num_hidden_layers: int = 12,
+        device: str = "cuda",
     ):
         self.local_files_only = bool(local_files_only)
 
         if uri_tokenizer is None:
             uri_tokenizer = uri_model
+
+        if device == "cuda" and not torch.cuda.is_available():
+            device = "cpu"
+
+        config = transformers.BertConfig.from_pretrained(uri_model)
+        config.max_position_embeddings = 1024
+        config.num_hidden_layers = num_hidden_layers
+        config.num_labels = num_labels
 
         tokenizer = transformers.AutoTokenizer.from_pretrained(
             uri_tokenizer,
@@ -28,12 +40,35 @@ class Segmenter:
             cache_dir="../cache/tokenizers",
         )
 
+        model = transformers.AutoModelForTokenClassification.from_config(config)
+
+        model = model.to(device)
+
+        """
         model = transformers.AutoModelForTokenClassification.from_pretrained(
             uri_model,
             num_labels=num_labels,
             local_files_only=self.local_files_only,
             cache_dir="../cache/models",
+            num_hidden_layers=num_hidden_layers,
         )
+
+        emb = model.bert.embeddings.position_embeddings
+        emb = torch.nn.Embedding.from_pretrained(
+            torch.cat(
+                [
+                    emb.weight,
+                    torch.zeros(
+                        1024 - emb.num_embeddings, 768, device=emb.weight.device
+                    ),
+                ],
+                axis=0,
+            )
+        )
+        tokenizer.model_max_length = 1024
+        model.config.max_position_embeddings = 1024
+        model.bert.embeddings.position_embeddings = emb
+        """
 
         self.pipeline = transformers.pipeline(
             "token-classification", model=model, tokenizer=tokenizer
