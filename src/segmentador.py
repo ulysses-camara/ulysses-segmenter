72d1a268347c1d859191761b384300c87190f853
felsiq <felipe.siqueira@usp.br>
2022-03-11 14:25:23 -0300

    doc: improve segmenter module documentation

--- src/segmentador.py
+++ src/segmentador.py
@@ -9,7 +9,61 @@ import numpy as np
 
 
 class Segmenter:
-    """TODO."""
+    r"""Brazilian Portuguese legal text segmenter class.
+
+    Uses a pretrained Transformer Encoder to segment Brazilian Portuguese legal texts.
+    The pretrained models support texts up to 1024 subwords. Texts larger than this
+    value are pre-segmented into 1024 subword blocks, and each block is feed to the
+    segmenter individually.
+
+    Parameters
+    ----------
+    uri_model : str, default="neuralmind/bert-base-portuguese-cased"
+        URI to load pretrained model from. May be a Hugginface HUB URL (if
+        `local_files_only=False`) or a local file.
+
+    uri_tokenizer : str or None, default=None
+        URI to pretrained text Tokenizer. If None, will load the tokenizer from
+        the `uri_model` path.
+
+    local_files_only : bool, default=True
+        If True, will search only for local pretrained model and tokenizers.
+        If False, may download models from Huggingface HUB, if necessary.
+
+    device : {'cpu', 'cuda'}, default="cpu"
+        Device to segment document content.
+
+    init_from_pretrained_weights : bool, default=True
+        if True, load pretrained weights from the specified `uri_model` argument.
+        If False, load only the model configuration from the same argument.
+
+    config : t.Optional[transformers.BertConfig], default=None
+        Custom model configuration. Used only if `init_from_pretrained_weights=False`.
+        If `init_from_pretrained_weights=False` and `config=None`, will load the
+        configuration file from `uri_model` with the following changes:
+        - config.max_position_embeddings = 1024
+        - config.num_hidden_layers = num_hidden_layers
+        - config.num_labels = num_labels
+
+    num_labels : int, default=4
+        Number of labels in the configuration file.
+
+    num_hidden_layers : int, default=6
+        Number of maximum Transformer Encoder hidden layers. If the model has more hidden
+        layers than the specified value in this parameter, later hidden layers will be
+        removed.
+
+    cache_dir_model : str, default="../cache/models"
+        Cache directory for transformer encoder model.
+
+    cache_dir_tokenizer : str, default="../cache/tokenizers"
+        Cache directory for text tokenizer.
+
+    regex_justificativa : str, regex.Pattern or None, default=None
+        Regular expression specifying how the `justificativa` portion from legal
+        documents should be detected. If None, will use the pattern predefined in
+        `Segmenter.RE_JUSTIFICATIVA` class attribute.
+    """
 
     RE_BLANK_SPACES = regex.compile(r"\s+")
     RE_JUSTIFICATIVA = regex.compile(
@@ -26,13 +80,14 @@ class Segmenter:
         self,
         uri_model: str = "neuralmind/bert-base-portuguese-cased",
         uri_tokenizer: t.Optional[str] = None,
-        num_labels: int = 4,
         local_files_only: bool = True,
-        device: str = "cuda",
-        init_from_pretrained_weights: bool = False,
+        device: str = "cpu",
+        init_from_pretrained_weights: bool = True,
         config: t.Optional[transformers.BertConfig] = None,
+        num_labels: int = 4,
         num_hidden_layers: int = 6,
-        cache_dir: str = "../cache/models",
+        cache_dir_model: str = "../cache/models",
+        cache_dir_tokenizer: str = "../cache/tokenizers",
         regex_justificativa: t.Optional[t.Union[str, regex.Pattern]] = None,
     ):
         self.local_files_only = bool(local_files_only)
@@ -53,7 +108,7 @@ class Segmenter:
             model = transformers.AutoModelForTokenClassification.from_pretrained(
                 uri_model,
                 local_files_only=self.local_files_only,
-                cache_dir=cache_dir,
+                cache_dir=cache_dir_model,
             )
 
         else:
@@ -62,7 +117,7 @@ class Segmenter:
         tokenizer = transformers.AutoTokenizer.from_pretrained(
             uri_tokenizer,
             local_files_only=self.local_files_only,
-            cache_dir="../cache/tokenizers",
+            cache_dir=cache_dir_tokenizer,
         )
 
         model.resize_token_embeddings(tokenizer.vocab_size)
@@ -87,6 +142,7 @@ class Segmenter:
         return self._tokenizer
 
     def save_pretrained(self, save_directory: str) -> None:
+        """Save pipeline (model and tokenizer) in `save_directory` path."""
         self.pipeline.save_pretrained(save_directory)
 
     @classmethod
@@ -109,7 +165,36 @@ class Segmenter:
         return_justificativa: bool = False,
         regex_justificativa: t.Optional[t.Union[str, regex.Pattern]] = None,
     ) -> t.Union[str, tuple[str, list[str]]]:
-        """Apply minimal legal text preprocessing."""
+        """Apply minimal legal text preprocessing.
+
+        The preprocessing steps are:
+        1. Coalesce all blank spaces in text;
+        2. Remove all trailing and leading blank spaces; and
+        3. Pre-segment text into legal text content and `justificativa`.
+
+        Parameters
+        ----------
+        text : str
+            Text to be preprocessed.
+
+        return_justificativa : bool, default=False
+            If True, return a tuple in the format (content, justificativa).
+            If False, return only `content`.
+
+        regex_justificativa : str, regex.Pattern or None, default=None
+            Regular expression specifying how the `justificativa` portion from legal
+            documents should be detected. If None, will use the pattern predefined in
+            `Segmenter.RE_JUSTIFICATIVA` class attribute.
+
+        Returns
+        -------
+        preprocessed_text : str
+            Content from `text` after the preprocessing steps.
+
+        justificativa_block : list[str]
+            Detected legal text `justificativa` blocks.
+            Only returned if `return_justificativa=True`.
+        """
         text = cls.RE_BLANK_SPACES.sub(" ", text)
         text = text.strip()
 
@@ -126,7 +211,30 @@ class Segmenter:
         text: str,
         return_justificativa: bool = False,
     ) -> t.Union[list[str], tuple[list[str], list[str]]]:
-        """Segment `text`."""
+        """Segment legal `text`.
+
+        The pretrained model support texts up to 1024 subwords. Texts larger than this
+        value are pre-segmented into 1024 subword blocks, and each block is feed to the
+        segmenter individually.
+
+        Parameters
+        ----------
+        text : str
+            Legal text to be segmented.
+
+        return_justificativa : bool, default=False
+            If True, return a tuple in the format (content, justificativa).
+            If False, return only `content`.
+
+        Returns
+        -------
+        preprocessed_text : list[str]
+            Content from `text` after the preprocessing steps.
+
+        justificativa_block : list[str]
+            Detected legal text `justificativa` blocks.
+            Only returned if `return_justificativa=True`.
+        """
         self._model.eval()
 
         preproc_result = self.preprocess_legal_text(
@@ -185,5 +293,7 @@ class Segmenter:
 
         return segs
 
-    def __call__(self, *args, **kwargs) -> t.Union[list[str], tuple[list[str], list[str]]]:
+    def __call__(
+        self, *args, **kwargs
+    ) -> t.Union[list[str], tuple[list[str], list[str]]]:
         return self.segment_legal_text(*args, **kwargs)
