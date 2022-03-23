b49034ca0eb4833451fec8c34979355ffc398fe2
felsiq <felipe.siqueira@usp.br>
2022-03-23 20:41:58 -0300

    update: add margin computation in interative_labeling/python_api.py

--- notebooks/interactive_labeling/python_api.py
+++ notebooks/interactive_labeling/python_api.py
@@ -1,15 +1,40 @@
+import os
 import typing as t
 import json as json_package
 import collections
 
+import numpy as np
+import numpy.typing as npt
 import requests
+
 
 FLASK_PORT = 5000
 DATA_WAS_SENT = False
 
 
-def open_example(tokens: list[str], labels: list[t.Union[int, str]]) -> None:
+def _compute_margin(logits: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    def softmax(x):
+        x = np.array(x, dtype=np.float64)
+        x -= np.max(x, axis=-1, keepdims=True)
+        x -= np.log(np.sum(np.exp(x), axis=-1, keepdims=True))
+        return np.exp(x)
+
+    activations = softmax(logits)
+    rank_1, rank_2 = np.quantile(activations, q=(1.0, 0.75), interpolation="nearest", axis=-1)
+    return (rank_1 - rank_2).squeeze()
+
+
+def open_example(
+    tokens: list[str],
+    labels: list[t.Union[int, str]],
+    logits: t.Optional[npt.NDArray[np.float64]] = None,
+) -> None:
     data = [{"token": tok, "label": lab} for tok, lab in zip(tokens, labels)]
+
+    if logits is not None:
+        margins = _compute_margin(logits)
+        for item, margin in zip(data, margins):
+            item["margin"] = margin
 
     rep = requests.post(
         os.path.join(f"http://localhost:{FLASK_PORT}/", "refinery-data-transfer"),
