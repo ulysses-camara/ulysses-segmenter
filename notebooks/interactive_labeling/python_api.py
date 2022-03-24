41d4ef26720e3307803b6f610fca4698e139c5d2
felsiq <felipe.siqueira@usp.br>
2022-03-24 12:58:12 -0300

    update: add 'apply_softmax_to_logits' argument in interactive_labeling/python_api.py

--- notebooks/interactive_labeling/python_api.py
+++ notebooks/interactive_labeling/python_api.py
@@ -12,14 +12,17 @@ FLASK_PORT = 5000
 DATA_WAS_SENT = False
 
 
-def _compute_margin(logits: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+def _compute_margin(
+    logits: npt.NDArray[np.float64],
+    apply_softmax_to_logits: bool = True,
+) -> npt.NDArray[np.float64]:
     def softmax(x):
         x = np.array(x, dtype=np.float64)
         x -= np.max(x, axis=-1, keepdims=True)
         x -= np.log(np.sum(np.exp(x), axis=-1, keepdims=True))
         return np.exp(x)
 
-    activations = softmax(logits)
+    activations = softmax(logits) if apply_softmax_to_logits else logits
     rank_1, rank_2 = np.quantile(activations, q=(1.0, 0.75), interpolation="nearest", axis=-1)
     return (rank_1 - rank_2).squeeze()
 
@@ -28,11 +31,12 @@ def open_example(
     tokens: list[str],
     labels: list[t.Union[int, str]],
     logits: t.Optional[npt.NDArray[np.float64]] = None,
+    apply_softmax_to_logits: bool = True,
 ) -> None:
     data = [{"token": tok, "label": lab} for tok, lab in zip(tokens, labels)]
 
     if logits is not None:
-        margins = _compute_margin(logits)
+        margins = _compute_margin(logits, apply_softmax_to_logits=apply_softmax_to_logits)
         for item, margin in zip(data, margins):
             item["margin"] = margin
 
