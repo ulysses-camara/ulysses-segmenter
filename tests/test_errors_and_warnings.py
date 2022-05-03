68a91d447012c8c668339778363f5a01638d6d23
felsiq <felipe.siqueira@usp.br>
2022-05-02 22:17:50 -0300

    tests: add tests for repeated file URIs in ONNX quantization

--- tests/test_errors_and_warnings.py
+++ tests/test_errors_and_warnings.py
@@ -1,4 +1,5 @@
 """Tests that are expected to fail, raise expections or warnings."""
+import typing as t
 import pytest
 
 import segmentador
@@ -99,4 +100,54 @@ def test_invalid_onnx_format_for_pruned_bert(fixture_test_paths: paths.TestPaths
         segmentador.optimize.quantize_model(
             model=model_pruned,
             model_output_format="onnx",
+        )
+
+
+@pytest.mark.parametrize(
+    "quantized_model_filename,intermediary_onnx_model_name,intermediary_onnx_optimized_model_name",
+    [
+        ("redundant_name", "redundant_name", None),
+        (None, "redundant_name", "redundant_name"),
+        ("redundant_name", None, "redundant_name"),
+        ("redundant_name", "redundant_name", "redundant_name"),
+    ],
+)
+def test_repeated_file_uris_in_onnx_quantization_lstm(
+    fixture_model_lstm_1_layer: segmentador.LSTMSegmenter,
+    quantized_model_filename: t.Optional[str],
+    intermediary_onnx_model_name: t.Optional[str],
+    intermediary_onnx_optimized_model_name: t.Optional[str],
+):
+    with pytest.raises(ValueError):
+        segmentador.optimize.quantize_model(
+            model=fixture_model_lstm_1_layer,
+            model_output_format="onnx",
+            quantized_model_filename=quantized_model_filename,
+            intermediary_onnx_model_name=intermediary_onnx_model_name,
+            intermediary_onnx_optimized_model_name=intermediary_onnx_optimized_model_name,
+        )
+
+
+@pytest.mark.parametrize(
+    "quantized_model_filename,intermediary_onnx_model_name,intermediary_onnx_optimized_model_name",
+    [
+        ("redundant_name", "redundant_name", None),
+        (None, "redundant_name", "redundant_name"),
+        ("redundant_name", None, "redundant_name"),
+        ("redundant_name", "redundant_name", "redundant_name"),
+    ],
+)
+def test_repeated_file_uris_in_onnx_quantization_bert(
+    fixture_model_bert_2_layers: segmentador.Segmenter,
+    quantized_model_filename: t.Optional[str],
+    intermediary_onnx_model_name: t.Optional[str],
+    intermediary_onnx_optimized_model_name: t.Optional[str],
+):
+    with pytest.raises(ValueError):
+        segmentador.optimize.quantize_model(
+            model=fixture_model_bert_2_layers,
+            model_output_format="onnx",
+            quantized_model_filename=quantized_model_filename,
+            intermediary_onnx_model_name=intermediary_onnx_model_name,
+            intermediary_onnx_optimized_model_name=intermediary_onnx_optimized_model_name,
         )
