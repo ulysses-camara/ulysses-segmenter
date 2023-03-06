2d6db690fc6a1c3e9965d0cb3bed4d965a780e24
felsiq <felipe.siqueira@usp.br>
2023-03-06 12:52:05 -0300

    update: register segmenter v2 model URIs in default parameters

--- src/segmentador/segmenter.py
+++ src/segmentador/segmenter.py
@@ -26,7 +26,7 @@ class BERTSegmenter(_base.BaseSegmenter):
 
     Parameters
     ----------
-    uri_model : str, default='2_layer_6000_vocab_size_bert'
+    uri_model : str, default='2_layer_6000_vocab_size_bert_v2'
         URI to load pretrained model from. May be a valid pretrained Ulysses segmenter model,
         a Hugginface HUB URL, or a local file (mandatory when `local_files_only=True`).
         See [1]_ for more information about pretrained Ulysses segmenter models.
@@ -104,7 +104,7 @@ class BERTSegmenter(_base.BaseSegmenter):
 
     def __init__(
         self,
-        uri_model: str = "2_layer_6000_vocab_size_bert",
+        uri_model: str = "2_layer_6000_vocab_size_bert_v2",
         uri_tokenizer: t.Optional[str] = None,
         inference_pooling_operation: str = "sum",
         local_files_only: bool = False,
@@ -169,7 +169,7 @@ class LSTMSegmenter(_base.BaseSegmenter):
 
     Parameters
     ----------
-    uri_model : str, default='512_hidden_dim_6000_vocab_size_1_layer_lstm'
+    uri_model : str, default='256_hidden_dim_6000_vocab_size_1_layer_lstm_v2'
         URI to load pretrained model from. May be a valid pretrained Ulysses segmenter model, or a
         local file (mandatory when `local_files_only=True`). See [1]_ for more information about
         pretrained Ulysses segmenter models.
@@ -242,7 +242,7 @@ class LSTMSegmenter(_base.BaseSegmenter):
 
     def __init__(
         self,
-        uri_model: str = "512_hidden_dim_6000_vocab_size_1_layer_lstm",
+        uri_model: str = "256_hidden_dim_6000_vocab_size_1_layer_lstm_v2",
         uri_tokenizer: str = "6000_subword_tokenizer",
         inference_pooling_operation: str = "sum",
         local_files_only: bool = False,
