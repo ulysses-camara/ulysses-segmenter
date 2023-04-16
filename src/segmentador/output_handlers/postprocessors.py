3625951ab46e9c507e6c9fce402479ac5f1cb6a5
felsiq <felipe.siqueira@usp.br>
2023-04-16 09:38:52 -0300

    [no ci] update: add 'à' in 'reg_spurious_whitespaces_dash_between_letters'

--- src/segmentador/output_handlers/postprocessors.py
+++ src/segmentador/output_handlers/postprocessors.py
@@ -12,17 +12,24 @@ reg_spurious_whitespaces_punct = re.compile(
     r"(?<=[(\[{/\\\u201C\u2018])\s+|\s+(?=[:?!,.;)\]}/\\\u2019\u201D])"
 )
 reg_spurious_whitespaces_dash_between_letters = re.compile(
-    r"(?<=[a-záéíóúãẽõâêôüç])\s*-\s*(?=[a-záéíóúãẽõâêôüç])"
+    r"(?<=[a-záéíóúãẽõâêôüçà])\s*-\s*(?=[a-záéíóúãẽõâêôüçà])"
 )
 reg_spurious_whitespaces_dot_between_numbers = re.compile(r"(?<=[0-9])\s*\.\s*(?=[0-9])")
 reg_spurious_whitespaces_between_pars = re.compile(r"(?<=§)\s+(?=§)")
+reg_spurious_whitespaces_between_dots = re.compile(r"(?<=\.)\s+(?=\.)")
 
 
-def remove_spurious_whitespaces_(segs: t.List[str]) -> None:
-    """Remove unnecessary whitespaces injected by the segmenter tokenizer."""
+def remove_spurious_whitespaces_(segs: t.List[str]) -> t.List[str]:
+    """Remove unnecessary whitespaces injected by the segmenter tokenizer.
+
+    The input list is modified in-place.
+    """
     for i, text in enumerate(segs):
         text = reg_spurious_whitespaces_punct.sub("", text)
         text = reg_spurious_whitespaces_dash_between_letters.sub("-", text)
         text = reg_spurious_whitespaces_dot_between_numbers.sub(".", text)
         text = reg_spurious_whitespaces_between_pars.sub("", text)
+        text = reg_spurious_whitespaces_between_dots.sub("", text)
         segs[i] = text.strip()
+
+    return segs
